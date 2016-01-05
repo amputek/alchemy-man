c4ad41fa2d0bd49aa2e16f5d6fcc9c3a7fb01a4a
Rob Dawson <r.dawson.7291@googlemail.com>
2016-01-05 23:31:27 +0700

    Removed crafting table element from potion manager

--- js/singletons/PotionManager.js
+++ js/singletons/PotionManager.js
@@ -4,16 +4,14 @@ var PotionManager = Class.$extend({
     this.currentlySelected = "fire";
     this.crafting = null;
     this.craftTimer = 0;
+    this.craftDuration = 1;
     this.enabled = {fire:true,ice:true,acid:true,nuke:false};
-
-    // this.equip("fire")
-
   },
 
   update: function(){
     if(this.crafting != null){
       this.craftTimer++;
-      if(this.craftTimer > 1){
+      if(this.craftTimer > this.craftDuration){
         this.equip(this.crafting);
         this.crafting = null;
       }
@@ -21,21 +19,7 @@ var PotionManager = Class.$extend({
     this.cooldown--;
   },
 
-  enable: function(type){
-    if(type == "fire") this.enabled.fire = true;
-    if(type == "ice" ) this.enabled.ice  = true;
-    if(type == "acid") this.enabled.acid = true;
-
-    debug.log("You've learnt how to make " + type + " potions!!")
-    this.currentlySelected = type;
-  },
-
-  pickup: function(type){
-
-  },
-
   make: function(start,type){
-
     if(start == true && this.crafting == null && this.currentlySelected != type){
       if(type != this.crafting){
         var success = true;
@@ -65,18 +49,14 @@ var PotionManager = Class.$extend({
     if(type == "fire"){
       input.reticule.style.border = "1px solid rgb(255,50,20)";
       input.reticule.style.boxShadow = "0 0 0px rgba(255,50,20, 1.0)";
-
-      // input.reticule.children[0].style.background = "rgb(255,50,20)";
     }
     if(type == "ice"){
       input.reticule.style.border = "1px solid rgb(50,155,255)";
       input.reticule.style.boxShadow = "0 0 0px rgba(50,155,255, 1.0)";
-      // input.reticule.children[0].style.background = "rgb(50,155,255)";
     }
     if(type == "acid"){
       input.reticule.style.border = "1px solid rgb(100,255,50)";
       input.reticule.style.boxShadow = "0 0 0px rgba(100,255,50, 1.0)";
-      // input.reticule.children[0].style.background = "rgb(100,255,50)";
     }
 
     mouseBlips.add( new MouseBlip( input.mousepos, this.currentlySelected) )
